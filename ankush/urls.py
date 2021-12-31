cfdbf890b887ccd6848af80fbfe392ce09b16308
itsankushsingh <96742942+itsankushsingh@users.noreply.github.com>
2021-12-31 20:19:36 +0530

    Update urls.py

--- ankush/urls.py
+++ ankush/urls.py
@@ -19,5 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.ankush, name='home'),
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze'),
+    path('about', views.about, name='about'),
 ]
